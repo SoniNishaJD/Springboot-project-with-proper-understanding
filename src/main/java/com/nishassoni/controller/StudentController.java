4f2da9f93f18354285b3f0ad768d96b84cb9a315
Soni Nisha <soni.nishaa24@gmail.com>
2023-09-02 00:15:56 +0530

    9 Sept

--- src/main/java/com/nishassoni/controller/StudentController.java
+++ src/main/java/com/nishassoni/controller/StudentController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nishassoni.beans.Student;
@@ -32,6 +35,20 @@ public class StudentController {
 	public Student studentPathVariable(@PathVariable("id") int studentId) {
 		return new Student(studentId,"Nisha", "Soni");
 		
+	}
+	
+	//Request Param 
+	@GetMapping("students/query")
+	public Student studentRequestVariable(@RequestParam int id) {
+		return new Student (id, "Nisha", "Soni");
+	}
+	
+	@PostMapping("students/create")
+	public Student createStudent(@RequestBody Student student) {
+		System.out.println(student.getId());
+		System.out.println(student.getFirstName());
+		System.out.println(student.getLasstName());
+		return student;
 	}
 
 }
